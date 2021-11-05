2dd6b1a34717c2cd0f5d653ce0b51eb3817104c3
Milan Pikula <milan.pikula@ipsec.info>
2021-11-05 12:05:23 +0100

    fix proxy+user agent in rss collector

--- src/collectors/collectors/rss_collector.py
+++ src/collectors/collectors/rss_collector.py
@@ -3,6 +3,8 @@ import hashlib
 import uuid
 
 import feedparser
+import urllib
+from urllib.request import ProxyHandler
 import requests
 from bs4 import BeautifulSoup
 from dateutil.parser import parse
@@ -17,9 +19,10 @@ class RSSCollector(BaseCollector):
     name = "RSS Collector"
     description = "Collector for gathering data from RSS feeds"
 
-    parameters = [Parameter(0, "FEED_URL", "Feed URL", "Full url for RSS feed", ParameterType.STRING),
-                  Parameter(0, "USER_AGENT", "User agent", "Type of user agent", ParameterType.STRING)
-                  ]
+    parameters = [
+        Parameter(0, "FEED_URL", "Feed URL", "Full url for RSS feed", ParameterType.STRING),
+        Parameter(0, "USER_AGENT", "User agent", "Type of user agent", ParameterType.STRING)
+    ]
 
     parameters.extend(BaseCollector.parameters)
 
@@ -28,23 +31,31 @@ class RSSCollector(BaseCollector):
     def collect(self, source):
 
         feed_url = source.parameter_values['FEED_URL']
-        user_agent = source.parameter_values['USER_AGENT']
         interval = source.parameter_values['REFRESH_INTERVAL']
+
+        user_agent = source.parameter_values['USER_AGENT']
+        if user_agent:
+            feedparser.USER_AGENT = user_agent
+            user_agent_headers = {'User-Agent': user_agent}
+        else:
+            user_agent_headers = { }
 
         proxies = {}
         if 'PROXY_SERVER' in source.parameter_values:
             proxy_server = source.parameter_values['PROXY_SERVER']
+
             if proxy_server.startswith('https://'):
-                proxies['https'] = proxy_server
+                proxies['https'] = proxy_server[8:].rstrip('/')
             elif proxy_server.startswith('http://'):
-                proxies['http'] = proxy_server
+                proxies['http'] = proxy_server[7:].rstrip('/')
             else:
-                proxies['http'] = 'http://' + proxy_server
+                proxies['http'] = proxy_server.rstrip('/')
+
 
         try:
             if proxies:
-                rss_xml = requests.get(feed_url, headers={'User-Agent': user_agent}, proxies=proxies)
-                feed = feedparser.parse(rss_xml.text)
+                proxy = ProxyHandler(proxies)
+                feed = feedparser.parse(feed_url, handlers = [proxy])
             else:
                 feed = feedparser.parse(feed_url)
 
@@ -64,9 +75,9 @@ class RSSCollector(BaseCollector):
                 link_for_article = feed_entry['link']
 
                 if proxies:
-                    page = requests.get(link_for_article, headers={'User-Agent': user_agent}, proxies=proxies)
+                    page = requests.get(link_for_article, headers=user_agent_headers, proxies=proxies)
                 else:
-                    page = requests.get(link_for_article, headers={'User-Agent': user_agent})
+                    page = requests.get(link_for_article, headers=user_agent_headers)
 
                 html_content = page.text
                 soup = BeautifulSoup(html_content, features='html.parser')
