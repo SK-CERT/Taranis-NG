1bdc889e492e193169983e8561087624ef132fc9
Lenhard Reuter <lenhard.reuter@ait.ac.at>
2022-04-26 16:59:36 +0200

    Add tag-list

---
+++ src/gui/src/components/common/tags/TagList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <v-btn 
+        v-for="tag in tags" :key="tag.label"
+          depressed 
+          small dark left>
+          {{ tag.label }}
+      </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TagList',
+  props: {
+    tags: []
+  },
+  data: () => ({
+  }),
+  computed: {
+  }
+}
+</script>
