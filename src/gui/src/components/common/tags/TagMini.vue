bba2fd311c8dc7542bd0853a4e5a201cf222cd1c
Lenhard Reuter <lenhard.reuter@ait.ac.at>
2022-04-26 16:59:23 +0200

    Add mini-tag

---
+++ src/gui/src/components/common/tags/TagMini.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-btn 
+        depressed 
+        x-small dark left mx="1"
+        class="text-caption text-weight-bold text-uppercase tag-mini">
+        {{ label }}
+    </v-btn>
+</template>
+
+<script>
+export default {
+  name: 'TagMini',
+  props: {
+    label: String
+  },
+  data: () => ({
+  }),
+  computed: {
+  }
+}
+</script>
