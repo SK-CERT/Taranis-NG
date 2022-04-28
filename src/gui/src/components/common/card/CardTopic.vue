7e116c76bd590a69c4521603293721b41e24e1f2
Lenhard Reuter <lenhard.reuter@ait.ac.at>
2022-04-28 12:43:31 +0200

    Add behaviour to cards

--- src/gui/src/components/common/card/CardTopic.vue
+++ src/gui/src/components/common/card/CardTopic.vue
@@ -1,5 +1,12 @@
 <template>
-    <v-card tile elevation="4" outlined height="100%" class="pa-5 align-self-stretch">
+    <v-card tile elevation="4" outlined height="100%" :class="[
+    'pa-5',
+    'pl-1', 
+    'align-self-stretch',
+    'topic',
+    {
+        'hot-topic': topic.hot,
+    }]">
         <v-container column style="height: 100%">
 
             <v-row no-gutters style="height: 100%">
@@ -9,14 +16,13 @@
 
                     <v-row class="flex-grow-0">
                         <v-col cols="auto" class="mr-auto mt-1" order-sm="2" order-md="1">
-                            <tag-mini label="AI"/>
+                            <tag-mini label="AI" v-show="topic.ai" />
                             <span class="text-caption font-weight-light main-text-color--text">Last activity: </span>
                             <span class="text-caption font-weight-bold main-text-color--text">March, 15, 2022</span>
                         </v-col>
 
                         <v-col cols="auto" order-sm="1" order-md="2">
-                            <v-btn fab depressed x-small dark color="primary" class="fab-pin">
-                                <!-- <v-icon>mdi-pin-outline</v-icon> -->
+                            <v-btn fab depressed outlined x-small color="grey" :class="['fab-pin', {'pinned': topic.pinned,}]" @click="topic.pinned = !topic.pinned">
                                 <v-icon>$awakePin</v-icon>
                             </v-btn>
                         </v-col>
@@ -24,62 +30,62 @@
 
                     <!-- Title -->
             
-                    <v-row class="flex-grow-0">
+                    <v-row class="flex-grow-0 mt-1">
                         <v-col>
                             <h2 class="font-weight-bold headline main-text-color--text">
-                                {{ title }}
+                                {{ topic.title }}
                             </h2>
                         </v-col>
                     </v-row>
 
-                    <v-row class="flex-grow-0">
+                    <v-row class="flex-grow-0 mt-1">
                         <v-col>
                             <tag-topic v-for="tag in topic.tags" :key="tag.label" :tag="tag" />
-                        </v-col>
-                    </v-row>
-                    
-                    <!-- Excerpt -->
-            
-                    <v-row class="flex-grow-0">
-                        <v-col>
-                            <p class="font-weight-light main-text-color--text">
-                                {{ topic.summary }}
-                            </p>
                         </v-col>
                     </v-row>
 
                     <!-- spacer -->
 
                     <v-spacer></v-spacer>
+                    
+                    <!-- Excerpt -->
+            
+                    <v-row class="flex-grow-0 mt-2">
+                        <v-col>
+                            <p class="font-weight-light main-text-color--text topic-excerpt">
+                                {{ topic.summary }}
+                            </p>
+                        </v-col>
+                    </v-row>
 
                     <!-- Footer -->
 
-                    <v-row no-gutter wrap align="center" justify="end" class="flex-grow-0">
-                        <v-col md="12" lg="8" class="mx-0 d-flex justify-start">
+                    <v-row no-gutter wrap align="center" justify="end" class="flex-grow-0 mt-0">
+                        <v-col cols="12" md="8" class="mx-0 d-flex justify-start">
                             <v-container class="mx-0 pa-0">
                                 <v-row class="mx-0">
-                                    <v-col sm="6" class="pa-0 pt-0 pr-1">
+                                    <v-col cols="6" class="pa-0 pt-0 pr-1">
                                         <v-icon left small>mdi-file-outline</v-icon>
                                         <span class="text-caption font-weight-light main-text-color--text">92 /</span> 
                                         <span class="text-caption font-weight-bold main-text-color--text">35</span> 
                                     </v-col>
-                                    <v-col sm="6" class="pa-0 pt-0 pr-1">
+                                    <v-col cols="6" class="pa-0 pt-0 pr-1">
                                         <v-icon left small color="awake-green-color">mdi-arrow-up-circle-outline</v-icon>
                                         <span class="text-caption font-weight-light main-text-color--text">356</span> 
                                     </v-col>
-                                    <v-col sm="6" class="pa-0 pt-0 pr-1">
+                                    <v-col cols="6" class="pa-0 pt-0 pr-1">
                                         <v-icon left small>mdi-message-outline</v-icon>
                                         <span class="text-caption font-weight-light main-text-color--text">92 /</span> 
                                         <span class="text-caption font-weight-bold main-text-color--text">35</span> 
                                     </v-col>
-                                    <v-col sm="6" class="pa-0 pt-0 pr-1">
+                                    <v-col cols="6" class="pa-0 pt-0 pr-1">
                                         <v-icon left small color="awake-red-color">mdi-arrow-down-circle-outline</v-icon>
                                         <span class="text-caption font-weight-light main-text-color--text">92</span> 
                                     </v-col>
                                 </v-row>
                             </v-container>
                         </v-col>
-                        <v-col md="12" lg="4" class="mx-0 d-flex justify-end">
+                        <v-col cols="12" md="4" class="mx-0 d-flex justify-end">
                             <v-btn outlined class="text-lowercase btn-view-topic mt-1">
                                 <v-icon left>mdi-eye-outline</v-icon>
                                 view topic
@@ -105,7 +111,6 @@ export default {
     TagTopic,
   },
   props: {
-    title: String,
     topic: {}
   },
   data: () => ({
