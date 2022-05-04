66e81b32dfa404e439c860bfcbe8cfe0e1f1ffdd
Lenhard Reuter <lenhard.reuter@ait.ac.at>
2022-05-04 17:12:12 +0200

    Fix card sizes on large screens

--- src/gui/src/components/layouts/AdaptiveCardsize.vue
+++ src/gui/src/components/layouts/AdaptiveCardsize.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-col cols="12" sm="6" :md="promValue" :xl="(promValue-1)">
+    <v-col xs="12" sm="6" md="4" :xl="promValue">
         <card-topic :topic="topic" :topicList="topicList"></card-topic>
     </v-col>
 </template>
@@ -20,8 +20,13 @@ export default {
   emits: ['updatePinned'],
   computed: {
     promValue () {
-      var limit = ((this.$vuetify.breakpoint.xl) ? 4 : 3)
-      return (((this.position + 1) > limit) ? 3 : 4)
+      if (this.$vuetify.breakpoint.xl) {
+        // On extra large displays highlight top 4
+        return (this.position < 4) ? 3 : 2
+      } else {
+        // On smaller displays highlight top 3
+        return (this.position < 3) ? 4 : 3
+      }
     }
   }
 }
