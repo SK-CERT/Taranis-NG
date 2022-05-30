46893f7f24d4efd634f9531b56762a2828e5667b
Lenhard Reuter <lenhard.reuter@ait.ac.at>
2022-05-31 00:13:59 +0200

    Adapt padding

--- src/gui/src/components/layouts/ViewLayout.vue
+++ src/gui/src/components/layouts/ViewLayout.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <v-container fluid class="ma-0 pa-5 topic-header-container">
+    <v-container fluid class="ma-0 pa-0 topic-header-container">
       <v-row class="view-panel" dense>
         <slot name="panel"></slot>
       </v-row>
     </v-container>
     <v-container>
-      <div class="ma-5 mt-0 pa-5 pt-0">
+      <div class="ma-5 mt-5 pa-5 pt-0">
         <v-row class="view-content" dense>
           <slot name="content"></slot>
         </v-row>
