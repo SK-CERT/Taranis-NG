9f59dfb7094f79c2ac7f3d15cdb19d2412803e41
gergelylengyelNBU <92225093+gergelylengyelNBU@users.noreply.github.com>
2022-05-30 11:02:31 +0200

    Changed span to div

--- src/gui/src/components/assess/CardAssess.vue
+++ src/gui/src/components/assess/CardAssess.vue
@@ -58,14 +58,14 @@
                                                    @click.stop="openCard">
                                                 <v-icon v-if="opened" left>mdi-arrow-down-drop-circle</v-icon>
                                                 <v-icon v-if="!opened" left>mdi-arrow-right-drop-circle</v-icon>
-                                                <span
-                                                    class="subtitle-2"> {{ $t('card_item.aggregated_items') }}: {{ card.news_items.length }}</span>
+                                                <div
+                                                    class="subtitle-2"> {{ $t('card_item.aggregated_items') }}: {{ card.news_items.length }}</div>
                                             </v-btn>
                                         </template>
                                         <template v-else>
-                                            <span  class="caption font-weight-bold px-0 mt-1 pb-0 pt-0 info--text">
+                                            <div  class="caption font-weight-bold px-0 mt-1 pb-0 pt-0 info--text">
                                                 {{ itemLink }}
-                                            </span>
+                                            </div>
                                         </template>
 
                                         <span class="caption font-weight-bold grey--text pl-2 pr-1">
@@ -87,48 +87,48 @@
                                             <v-row v-if="!multiSelectActive && !analyze_selector"
                                                    v-bind="UI.CARD.TOOLBAR.COMPACT" :style="UI.STYLE.card_toolbar_strip_bottom">
                                                 <v-col v-bind="UI.CARD.COL.TOOLS">
-                                                    <v-btn v-bind="size" v-if="singleAggregate && canAccess" icon
+                                                    <v-btn v-if="singleAggregate && canAccess" icon
                                                            @click.stop="cardItemToolbar('link')"
                                                            data-btn="link" :title="$t('assess.tooltip.open_source')">
                                                         <a class="alink" :href="card.news_items[0].news_item_data.link"
                                                            target="_blank" rel="noreferer">
-                                                            <v-icon v-bind="size" color="accent">mdi-open-in-app</v-icon>
+                                                            <v-icon color="accent">mdi-open-in-app</v-icon>
                                                         </a>
                                                     </v-btn>
-                                                    <v-btn v-bind="size" v-if="!singleAggregate && canModify" icon
+                                                    <v-btn v-if="!singleAggregate && canModify" icon
                                                            @click.stop="cardItemToolbar('ungroup')"
                                                            data-btn="ungroup"
                                                            :title="$t('assess.tooltip.ungroup_news_item')">
-                                                        <v-icon v-bind="size" color="accent">mdi-ungroup</v-icon>
+                                                        <v-icon color="accent">mdi-ungroup</v-icon>
                                                     </v-btn>
-                                                    <v-btn v-bind="size" v-if="canCreateReport" icon @click.stop="cardItemToolbar('new')"
+                                                    <v-btn v-if="canCreateReport" icon @click.stop="cardItemToolbar('new')"
                                                            :title="$t('assess.tooltip.analyze_item')"
                                                            data-btn="new">
-                                                        <v-icon v-bind="size" color="accent">mdi-file-outline</v-icon>
+                                                        <v-icon color="accent">mdi-file-outline</v-icon>
                                                     </v-btn>
-                                                    <v-btn v-bind="size" v-if="canModify" icon @click.stop="cardItemToolbar('read')"
+                                                    <v-btn v-if="canModify" icon @click.stop="cardItemToolbar('read')"
                                                            data-btn="read" :title="$t('assess.tooltip.read_item')">
-                                                        <v-icon v-bind="size" :color="buttonStatus(card.read)">mdi-eye</v-icon>
+                                                        <v-icon :color="buttonStatus(card.read)">mdi-eye</v-icon>
                                                     </v-btn>
-                                                    <v-btn v-bind="size" v-if="canModify" icon @click.stop="cardItemToolbar('important')"
+                                                    <v-btn v-if="canModify" icon @click.stop="cardItemToolbar('important')"
                                                            :title="$t('assess.tooltip.important_item')"
                                                            data-btn="important">
-                                                        <v-icon v-bind="size" :color="buttonStatus(card.important)">mdi-star</v-icon>
+                                                        <v-icon :color="buttonStatus(card.important)">mdi-star</v-icon>
                                                     </v-btn>
-                                                    <v-btn v-bind="size" v-if="canModify" icon @click.stop="cardItemToolbar('like')"
+                                                    <v-btn v-if="canModify" icon @click.stop="cardItemToolbar('like')"
                                                            data-btn="like" :title="$t('assess.tooltip.like_item')">
-                                                        <v-icon v-bind="size" :color="buttonStatus(card.me_like)">mdi-thumb-up</v-icon>
+                                                        <v-icon :color="buttonStatus(card.me_like)">mdi-thumb-up</v-icon>
                                                     </v-btn>
-                                                    <v-btn v-bind="size" v-if="canModify" icon @click.stop="cardItemToolbar('unlike')"
+                                                    <v-btn v-if="canModify" icon @click.stop="cardItemToolbar('unlike')"
                                                            :title="$t('assess.tooltip.dislike_item')"
                                                            data-btn="unlike">
-                                                        <v-icon v-bind="size" :color="buttonStatus(card.me_dislike)">mdi-thumb-down
+                                                        <v-icon :color="buttonStatus(card.me_dislike)">mdi-thumb-down
                                                         </v-icon>
                                                     </v-btn>
-                                                    <v-btn v-bind="size" v-if="canDelete" icon @click.stop="cardItemToolbar('delete')"
+                                                    <v-btn v-if="canDelete" icon @click.stop="cardItemToolbar('delete')"
                                                            :title="$t('assess.tooltip.delete_item')"
                                                            data-btn="delete">
-                                                        <v-icon v-bind="size" color="accent">{{ UI.ICON.DELETE }}</v-icon>
+                                                        <v-icon color="accent">{{ UI.ICON.DELETE }}</v-icon>
                                                     </v-btn>
                                                 </v-col>
                                             </v-row>
@@ -247,10 +247,6 @@ export default {
             } else {
                 return "new"
             }
-        },
-        size () {
-          const size = {xs:'x-small',sm:'small',lg:'large',xl:'x-large'}[this.$vuetify.breakpoint.name];
-          return size ? { [size]: true } : {}
         },
     },
     methods: {
