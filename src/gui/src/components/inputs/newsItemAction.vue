b5beb11eae1dfb99423944a91b33602a6a1dd2e1
Lenhard Reuter <lenhard.reuter@ait.ac.at>
2022-06-20 17:35:57 +0200

    Outsource news items action

---
+++ src/gui/src/components/inputs/newsItemAction.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-tooltip open-delay="1000" bottom :disabled="!tooltip">
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn
+        v-bind="attrs"
+        v-on="on"
+        icon
+        tile
+        class="news-item-action"
+        :class="[{ active: active }]"
+        @click.native.capture="execute($event)"
+      >
+        <v-icon> $newsItemActionRead </v-icon>
+      </v-btn>
+    </template>
+    <span>{{ tooltip }}</span>
+  </v-tooltip>
+</template>
+
+<script>
+export default {
+  name: 'newsItemAction',
+  props: {
+    active: Boolean,
+    tooltip: String
+  },
+  methods: {
+    execute (event) {
+      event.stopPropagation()
+      this.$emit('input', event)
+    }
+  }
+}
+</script>
