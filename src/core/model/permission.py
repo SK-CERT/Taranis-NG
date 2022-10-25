SK-CERT/Taranis-NG

commit 89c3876dea5965a6b29324962534f4acf0388a5c

Removed from logs some sqlalchemy "SAWarning: relationship ..."

src/core/model/permission.py
@@ -9,7 +9,7 @@ class Permission(db.Model):
     name = db.Column(db.String(), unique=True, nullable=False)
     description = db.Column(db.String())
 
-    roles = db.relationship('Role', secondary='role_permission')
+    roles = db.relationship('Role', secondary='role_permission', back_populates="permissions")
 
     @classmethod
     def find(cls, permission_id):

src/core/model/role.py
@@ -19,7 +19,7 @@ class Role(db.Model):
     name = db.Column(db.String(64), unique=True, nullable=False)
     description = db.Column(db.String())
 
-    permissions = db.relationship(Permission, secondary='role_permission')
+    permissions = db.relationship(Permission, secondary='role_permission', back_populates="roles")
 
     def __init__(self, id, name, description, permissions):
         self.id = None

src/core/model/asset.py
@@ -23,7 +23,7 @@ class AssetCpe(db.Model):
     value = db.Column(db.String())
 
     asset_id = db.Column(db.Integer, db.ForeignKey('asset.id'))
-    asset = db.relationship("Asset")
+    asset = db.relationship("Asset", back_populates="asset_cpes")
 
     def __init__(self, value):
         self.id = None
@@ -47,9 +47,9 @@ class Asset(db.Model):
     asset_group_id = db.Column(db.String, db.ForeignKey('asset_group.id'))
     asset_group = db.relationship("AssetGroup")
 
-    asset_cpes = db.relationship("AssetCpe", cascade="all, delete-orphan")
+    asset_cpes = db.relationship("AssetCpe", back_populates="asset", cascade="all, delete-orphan")
 
-    vulnerabilities = db.relationship("AssetVulnerability", cascade="all, delete-orphan")
+    vulnerabilities = db.relationship("AssetVulnerability", back_populates="asset", cascade="all, delete-orphan")
     vulnerabilities_count = db.Column(db.Integer, default=0)
 
     def __init__(self, id, name, serial, description, asset_group_id, asset_cpes):
@@ -211,7 +211,7 @@ class AssetVulnerability(db.Model):
     solved = db.Column(db.Boolean, default=False)
 
     asset_id = db.Column(db.Integer, db.ForeignKey('asset.id'))
-    asset = db.relationship("Asset")
+    asset = db.relationship("Asset", back_populates="vulnerabilities")
 
     report_item_id = db.Column(db.Integer, db.ForeignKey('report_item.id'))
     report_item = db.relationship("ReportItem")

src/core/model/report_item.py
@@ -108,7 +108,7 @@ class ReportItem(db.Model):
 
     attributes = db.relationship('ReportItemAttribute', back_populates="report_item", cascade="all, delete-orphan")
 
-    report_item_cpes = db.relationship("ReportItemCpe", cascade="all, delete-orphan")
+    report_item_cpes = db.relationship("ReportItemCpe", back_populates="report_item", cascade="all, delete-orphan")
 
     def __init__(self, id, uuid, title, title_prefix, report_item_type_id, news_item_aggregates, remote_report_items,
                  attributes, completed):
@@ -572,7 +572,7 @@ class ReportItemCpe(db.Model):
     value = db.Column(db.String())
 
     report_item_id = db.Column(db.Integer, db.ForeignKey('report_item.id'))
-    report_item = db.relationship("ReportItem")
+    report_item = db.relationship("ReportItem", back_populates="report_item_cpes")
 
     def __init__(self, value):
         self.id = None

commit e15ee6fde1abfa3503c1b89bb6bfd7e3fd600b56

Fixed carsh by editing users (caused by change https://github.com/SK-CERT/Taranis-NG/commit/89c3876dea5965a6b29324962534f4acf0388a5c)

src/core/model/permission.py
@@ -9,8 +9,6 @@ class Permission(db.Model):
     name = db.Column(db.String(), unique=True, nullable=False)
     description = db.Column(db.String())
 
-    roles = db.relationship('Role', secondary='role_permission', back_populates="permissions")
-
     @classmethod
     def find(cls, permission_id):
         permission = cls.query.get(permission_id)

src/core/model/role.py
@@ -19,7 +19,7 @@ class Role(db.Model):
     name = db.Column(db.String(64), unique=True, nullable=False)
     description = db.Column(db.String())
 
-    permissions = db.relationship(Permission, secondary='role_permission', back_populates="roles")
+    permissions = db.relationship(Permission, secondary='role_permission')
 
     def __init__(self, id, name, description, permissions):
         self.id = None