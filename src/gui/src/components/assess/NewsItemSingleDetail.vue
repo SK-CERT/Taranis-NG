2dd1521dda45d34387e110cf2b1bd29f886085ef
Milan Pikula <milan.pikula@ipsec.info>
2023-01-26 17:14:45 +0100

    properly display attributes in an aggregate

--- src/gui/src/components/assess/NewsItemSingleDetail.vue
+++ src/gui/src/components/assess/NewsItemSingleDetail.vue
@@ -123,9 +123,12 @@
                     </v-tab-item>
 
                     <v-tab-item value="tab-2" class="pa-5">
-                        <NewsItemAttribute v-for="attribute in news_item.attributes" :key="attribute.id"
-                                           :attribute="attribute"
-                                           :news_item_data="news_item.news_items[0].news_item_data"/>
+                        <div v-for="item in news_item.news_items" :key="item.id">
+                            <NewsItemAttribute v-for="attribute in item.news_item_data.attributes"
+                                    :key="attribute.id"
+                                    :attribute="attribute"
+                                    :news_item_data="news_item.news_items[0].news_item_data"/>
+                        </div>
                     </v-tab-item>
 
                     <v-tab-item value="tab-3" class="pa-5">
