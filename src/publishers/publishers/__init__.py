bc723d2f114eca8308fa72dfbaeda76f403e8d66
multiflexi <multi.flexi@seznam.cz>
2023-11-23 14:44:52 +0100

    SFTP publisher

--- src/publishers/publishers/__init__.py
+++ src/publishers/publishers/__init__.py
@@ -1,1 +1,15 @@
-__all__ = ['base_publisher', 'ftp_publisher', 'email_publisher', 'twitter_publisher', 'wordpress_publisher']
+"""
+This module contains the publishers package.
+
+The publishers package provides various classes for publishing data to different platforms, including FTP, SFTP, email,
+Twitter, and WordPress.
+
+Classes:
+- base_publisher: The base class for all publishers.
+- ftp_publisher: A publisher for FTP.
+- sftp_publisher: A publisher for SFTP.
+- email_publisher: A publisher for email.
+- twitter_publisher: A publisher for Twitter.
+- wordpress_publisher: A publisher for WordPress.
+"""
+__all__ = ["base_publisher", "ftp_publisher", "sftp_publisher", "email_publisher", "twitter_publisher", "wordpress_publisher"]
