1cab122945d38389f38d1efd0967850deae83b51
martindruzbacky <druzbacky.martin@gmail.com>
2024-05-15 15:09:57 +0200

    Fixed wrong file formating

--- docker/prestart_core.sh
+++ docker/prestart_core.sh
@@ -1,1 +1,18 @@
-#! /usr/bin/env shecho "Running inside /app/prestart.sh..."echo "Running sse forward in the background..."/usr/local/bin/forward --sender-port 5000 --client-port 5001 &echo "Running migrations..."/app/db_migration.py db upgrade headif [ `./manage.py collector --list | wc -l` = 0 -a x"$SKIP_DEFAULT_COLLECTOR" != "xtrue" ]; then    (    echo "Adding default collector"    ./manage.py collector --create --name "Default Docker Collector" --description "A local collector node configured as a part of Taranis NG default installation." --api-url "http://collectors/" --api-key "$COLLECTOR_PRESENTER_PUBLISHER_API_KEY"    ) &fiecho "Done."
+#! /usr/bin/env sh
+
+echo "Running inside /app/prestart.sh..."
+
+echo "Running sse forward in the background..."
+/usr/local/bin/forward --sender-port 5000 --client-port 5001 &
+
+echo "Running migrations..."
+/app/db_migration.py db upgrade head
+
+if [ `./manage.py collector --list | wc -l` = 0 -a x"$SKIP_DEFAULT_COLLECTOR" != "xtrue" ]; then
+    (
+    echo "Adding default collector"
+    ./manage.py collector --create --name "Default Docker Collector" --description "A local collector node configured as a part of Taranis NG default installation." --api-url "http://collectors/" --api-key "$COLLECTOR_PRESENTER_PUBLISHER_API_KEY"
+    ) &
+fi
+
+echo "Done."
