475a84d9c801bdf61ae0bcc241a1945927fa3eaa
Jan Polonsky <jan.polonsky@nbu.gov.sk>
2024-05-20 11:07:03 +0200

    resolving conflicts before merge (test 1 :)

--- docker/prestart_core.sh
+++ docker/prestart_core.sh
@@ -2,10 +2,13 @@
 
 echo "Running inside /app/prestart.sh..."
 
-echo "Running migrations..."
-/app/db_migration.py db upgrade head
+echo "Running sse forward in the background..."
+/usr/local/bin/forward --sender-port 5000 --client-port 5001 &
 
-if [ "$(./manage.py collector --list | wc -l)" -eq 0 ] && [ x"$SKIP_DEFAULT_COLLECTOR" != "xtrue" ]; then
+echo "Running migrations..."
+python /app/db_migration.py db upgrade head
+
+if [ "$(python ./manage.py collector --list | wc -l)" == 0 ] && [ x"$SKIP_DEFAULT_COLLECTOR" != "xtrue" ]; then
     (
     echo "Adding default collector"
     if [ -z "$API_KEY_FILE" ]; then
@@ -14,7 +17,7 @@ if [ "$(./manage.py collector --list | wc -l)" -eq 0 ] && [ x"$SKIP_DEFAULT_COLL
         echo "Reading API key from file..."
         API_KEY=$(cat "$API_KEY_FILE")
     fi
-    ./manage.py collector --create --name "Default Docker Collector" --description "A local collector node configured as a part of Taranis NG default installation." --api-url "http://collectors/" --api-key "$API_KEY"
+    python ./manage.py collector --create --name "Default Docker Collector" --description "A local collector node configured as a part of Taranis NG default installation." --api-url "http://collectors/" --api-key "$API_KEY"
     ) &
 fi
 
