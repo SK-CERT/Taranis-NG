9f047a9bca48aae541b5fc9a0e46bce935771cc7
multiflexi <multi.flexi@seznam.cz>
2024-08-23 18:02:36 +0200

    logging in bots

--- src/bots/bots/base_bot.py
+++ src/bots/bots/base_bot.py
@@ -1,53 +1,99 @@
+"""BaseBot class represents the base abstract type for all bots."""
+
 import datetime
 import traceback
 
 from managers import time_manager
-from managers.log_manager import log_debug
+from managers.log_manager import logger
 from shared.schema import bot, bot_preset
 from shared.schema.parameter import Parameter, ParameterType
 from remote.core_api import CoreApi
 
 
 class BaseBot:
+    """BaseBot class represents the base abstract type for all bots.
+
+    Attributes:
+        type (str): The type of the bot.
+        name (str): The name of the bot.
+        description (str): The description of the bot.
+        parameters (list): The list of parameters for the bot.
+    Methods:
+        __init__(): Initializes a new instance of the BaseBot class.
+        get_info(): Gets the information of the bot.
+        execute(source): Executes the bot's task.
+        execute_on_event(preset, event_type, data): Executes the bot's task based on an event.
+        process_event(event_type, data): Processes an event and executes the bot's task for each preset.
+        print_exception(preset, error): Prints the exception details for a bot preset.
+        history(interval): Returns the history limit based on the given interval.
+        initialize(): Initializes the bot and schedules its execution based on the presets.
+    """
+
     type = "BASE_BOT"
     name = "Base Bot"
     description = "Base abstract type for all bots"
 
     # real values are stored in 'parameter' table
     parameters = [
-        Parameter(0, "REFRESH_INTERVAL", "Refresh interval (0 to disable)", "How often and when is this bot doing its job. Examples:<ul><li>10 --- perform the task every 10 minutes</li><li>10:30 --- perform the task every day at 10:30</li><li>Tuesday,10:30 --- perform the task every Tuesday at 10:30</li></ul>",
-                  ParameterType.NUMBER)
+        Parameter(
+            0,
+            "REFRESH_INTERVAL",
+            "Refresh interval (0 to disable)",
+            "How often and when is this bot doing its job. Examples:<ul><li>10 --- perform the task every 10 minutes</li><li>10:30"
+            " --- perform the task every day at 10:30</li><li>Tuesday,10:30 --- perform the task every Tuesday at 10:30</li></ul>",
+            ParameterType.NUMBER,
+        )
     ]
 
     def __init__(self):
+        """Initialize the BaseBot class."""
         self.bot_presets = []
 
     def get_info(self):
+        """Return the information of the bot.
+
+        Returns:
+            (dict): The information of the bot.
+        """
         info_schema = bot.BotSchema()
         return info_schema.dump(self)
 
-    def execute(self, source):
-        pass
-
-    def execute_on_event(self, preset, event_type, data):
-        pass
-
     def process_event(self, event_type, data):
+        """Process an event by executing it on all bot presets.
+
+        Parameters:
+            event_type (str): The type of the event.
+            data (Any): The data associated with the event.
+        """
         for preset in self.bot_presets:
             self.execute_on_event(preset, event_type, data)
 
     @staticmethod
     def print_exception(preset, error):
-        print('Bot Preset:', preset.id, preset.name)
-        if str(error).startswith('b'):
-            print('ERROR: ' + str(error)[2:-1])
+        """Print the bot preset ID, name, error message, and traceback.
+
+        Parameters:
+            preset: The bot preset object.
+            error: The error message.
+        """
+        print("Bot Preset:", preset.id, preset.name)
+        if str(error).startswith("b"):
+            print(f"ERROR:{error[2:-1]}")
         else:
-            print('ERROR: ' + str(error))
-        print('TRACEBACK:', traceback.format_exc(), flush=True)
+            print(f"ERROR: {error}")
+        print("TRACEBACK:", traceback.format_exc(), flush=True)
 
     @staticmethod
     def history(interval):
-        if interval[0].isdigit() and ':' in interval:
+        """Generate a timestamp limit based on the given interval.
+
+        Parameters:
+            interval (str or int): The interval to calculate the limit from. It can be a string representing a time interval in
+                the format "X:Y" (where X is a number and Y is a unit of time) or an integer representing the number of minutes.
+        Returns:
+            (str): The timestamp limit in the format "%d.%m.%Y - %H:%M".
+        """
+        if interval[0].isdigit() and ":" in interval:
             limit = datetime.datetime.now() - datetime.timedelta(days=1)
             limit = limit.strftime("%d.%m.%Y - %H:%M")
         elif interval[0].isalpha():
@@ -66,6 +112,7 @@ class BaseBot:
         return limit
 
     def initialize(self):
+        """Initialize the bot by retrieving bot presets and scheduling jobs based on the preset intervals."""
         response, code = CoreApi.get_bots_presets(self.type)
         if code == 200 and response is not None:
             preset_schema = bot_preset.BotPresetSchemaBase(many=True)
@@ -74,35 +121,35 @@ class BaseBot:
             for preset in self.bot_presets:
                 interval = preset.parameter_values["REFRESH_INTERVAL"]
                 # do not schedule if no interval is set
-                if interval == '' or interval == '0':
-                    log_debug("scheduling '{}' disabled".format(str(preset.name)))
+                if interval == "" or interval == "0":
+                    logger.log_debug(f"scheduling '{preset.name}' disabled")
                     continue
-                 
+
                 self.execute(preset)
 
                 if interval:
-                    if interval[0].isdigit() and ':' in interval:
-                        log_debug("scheduling '{}' at: {}".format(str(preset.name), str(interval)))
+                    if interval[0].isdigit() and ":" in interval:
+                        logger.log_debug(f"scheduling '{preset.name}' at: {interval}")
                         time_manager.schedule_job_every_day(interval, self.execute, preset)
                     elif interval[0].isalpha():
-                        interval = interval.split(',')
+                        interval = interval.split(",")
                         day = interval[0].strip()
                         at = interval[1].strip()
-                        log_debug("scheduling '{}' at: {} {}".format(str(preset.name), str(day), str(at)))
-                        if day == 'Monday':
+                        logger.log_debug(f"scheduling '{preset.name}' at: {day} {at}")
+                        if day == "Monday":
                             time_manager.schedule_job_on_monday(at, self.execute, preset)
-                        elif day == 'Tuesday':
+                        elif day == "Tuesday":
                             time_manager.schedule_job_on_tuesday(at, self.execute, preset)
-                        elif day == 'Wednesday':
+                        elif day == "Wednesday":
                             time_manager.schedule_job_on_wednesday(at, self.execute, preset)
-                        elif day == 'Thursday':
+                        elif day == "Thursday":
                             time_manager.schedule_job_on_thursday(at, self.execute, preset)
-                        elif day == 'Friday':
+                        elif day == "Friday":
                             time_manager.schedule_job_on_friday(at, self.execute, preset)
-                        elif day == 'Saturday':
+                        elif day == "Saturday":
                             time_manager.schedule_job_on_saturday(at, self.execute, preset)
                         else:
                             time_manager.schedule_job_on_sunday(at, self.execute, preset)
                     else:
-                        log_debug("scheduling '{}' for {}".format(str(preset.name), int(interval)))
+                        logger.log_debug(f"scheduling '{preset.name}' for {interval}")
                         time_manager.schedule_job_minutes(int(interval), self.execute, preset)
--- src/bots/managers/log_manager.py
+++ src/bots/managers/log_manager.py
@@ -1,95 +1,12 @@
+"""This file contains the logger configuration for the bots module."""
+
 import os
-import socket
-import logging
-import logging.handlers
-import traceback
+from shared.log import TaranisLogger
 
-# setup Flask logger
-gunicorn_logger = logging.getLogger('gunicorn.error')
-gunicorn_logger.setLevel(logging.INFO)
-
-# setup syslog logger
-sys_logger = logging.getLogger('SysLogger')
-sys_logger.setLevel(logging.INFO)
+# setup logger level
+logging_level_str = os.environ.get("LOG_LEVEL", "INFO")
 
 # custom module ID to append to log messages
-if "MODULE_ID" in os.environ:
-    module_id = os.environ.get("MODULE_ID")
-else:
-    module_id = None
+module_id = os.environ.get("MODULE_ID", None)
 
-# increase logging level
-if "DEBUG" in os.environ and os.environ.get("DEBUG").lower() == "true":
-    gunicorn_logger.setLevel(logging.DEBUG)
-    sys_logger.setLevel(logging.DEBUG)
-
-# send a debug message
-def log_debug(message):
-    formatted_message = "[{}] {}".format(module_id,message)
-    gunicorn_logger.debug(formatted_message)
-    if sys_logger:
-        sys_logger.debug(formatted_message)
-
-# send a debug message
-def log_debug_trace(message = None):
-    formatted_message1 = "[{}] {}".format(module_id,message)
-    formatted_message2 = "[{}] {}".format(module_id,traceback.format_exc())
-
-    if message:
-        gunicorn_logger.debug(formatted_message1)
-    gunicorn_logger.debug(formatted_message2)
-    if sys_logger:
-        if message:
-            sys_logger.debug(formatted_message1)
-        sys_logger.debug(formatted_message2)
-
-# send an info message
-def log_info(message):
-    formatted_message = "[{}] {}".format(module_id,message)
-    gunicorn_logger.info(formatted_message)
-    if sys_logger:
-        sys_logger.info(formatted_message)
-
-# send an warning message
-def log_warning(message):
-    formatted_message = "[{}] {}".format(module_id,message)
-    gunicorn_logger.warning(formatted_message)
-    if sys_logger:
-        sys_logger.warning(formatted_message)
-
-# send a critical message
-def log_critical(message):
-    formatted_message = "[{}] {}".format(module_id,message)
-    gunicorn_logger.critical(formatted_message)
-    if sys_logger:
-        sys_logger.critical(formatted_message)
-
-# try to connect syslog logger
-if "SYSLOG_URL" in os.environ and "SYSLOG_PORT" in os.environ:
-    try:
-        sys_log_handler = logging.handlers.SysLogHandler(
-            address=(os.environ["SYSLOG_URL"], int(os.environ["SYSLOG_PORT"])),
-            socktype=socket.SOCK_STREAM)
-        sys_logger.addHandler(sys_log_handler)
-    except Exception as ex:
-        sys_logger = None
-        log_debug("Unable to connect to syslog server!")
-        log_debug(ex)
-elif "SYSLOG_ADDRESS" in os.environ:
-    try:
-        sys_log_handler = logging.handlers.SysLogHandler(address=os.environ["SYSLOG_ADDRESS"])
-        sys_logger.addHandler(sys_log_handler)
-    except Exception as ex:
-        sys_logger = None
-        log_debug("Unable to connect to syslog server!")
-        log_debug(ex)
-else: 
-    # disable log duplicities on the screen if we have SYSLOG disabled
-    sys_logger = None
-
-def log_system_activity(module, message):
-    log_info("[{}] {}".format(module, message))
-
-def log_bot_activity(bot, message):
-    log_text = "BOT '{}': {}".format(bot, message)
-    log_info(log_text)
+logger = TaranisLogger(module_id, logging_level_str, True, False, os.environ.get("SYSLOG_ADDRESS"))
