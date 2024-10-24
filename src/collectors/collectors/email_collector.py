1a2695edc22fc7d54a9e484e4bdea77e7a9f0901
multiflexi <multi.flexi@seznam.cz>
2024-10-24 16:18:43 +0200

    first version

--- src/collectors/collectors/email_collector.py
+++ src/collectors/collectors/email_collector.py
@@ -33,7 +33,7 @@ class EmailCollector(BaseCollector):
     description = "Collector for gathering data from emails"
 
     parameters = [
-        Parameter(0, "EMAIL_SERVER_TYPE", "Email server type", "Server type parameter means IMAP or POP3 email server", ParameterType.STRING),
+        Parameter(0, "EMAIL_SERVER_TYPE", "Email server type", "IMAP or POP3 protocol", ParameterType.STRING),
         Parameter(0, "EMAIL_SERVER_HOSTNAME", "Email server hostname", "Hostname of email server", ParameterType.STRING),
         Parameter(0, "EMAIL_SERVER_PORT", "Email server port", "Port of email server", ParameterType.NUMBER),
         Parameter(0, "EMAIL_USERNAME", "Username", "Username of email account", ParameterType.STRING),
@@ -61,6 +61,7 @@ class EmailCollector(BaseCollector):
         proxy_server = source.parameter_values["PROXY_SERVER"]
 
         def proxy_tunnel():
+            logger.debug(f"{self.collector_source} Establishing proxy tunnel")
             server = f"{email_server_hostname.lower()}"
             port = email_server_port
 
@@ -75,63 +76,73 @@ class EmailCollector(BaseCollector):
             s.send(str.encode(con))
             s.recv(4096)
 
-        def get_data():
+        def process_email(email_message):
             review = ""
             content = ""
-            url = ""
+            address = ""
             link = ""
             key = ""
-            value = ""
 
-            date_tuple = email.utils.parsedate_tz(email_message["Date"])
-            local_date = datetime.datetime.fromtimestamp(email.utils.mktime_tz(date_tuple))
-            published = f'{str(local_date.strftime("%a, %d %b %Y %H:%M:%S"))}'
-
-            author = str(email.header.make_header(email.header.decode_header(email_message["From"])))
             title = str(email.header.make_header(email.header.decode_header(email_message["Subject"])))
+            logger.debug(f"{self.collector_source} Processing email: {title}")
+            author = str(email.header.make_header(email.header.decode_header(email_message["From"])))
+            address = email.utils.parseaddr(email_message["From"])[1]
             message_id = str(email.header.make_header(email.header.decode_header(email_message["Message-ID"])))
+            date_tuple = email.utils.parsedate_tz(email_message["Date"])
+            published = datetime.datetime.fromtimestamp(email.utils.mktime_tz(date_tuple)).strftime("%d.%m.%Y - %H:%M")
 
             for part in email_message.walk():
                 if part.get_content_type() == "text/plain":
-                    content = part.get_payload(decode=True)
-                    review = content[:500].decode("utf-8")
-                    content = content.decode("utf-8")
+                    content = part.get_payload(decode=True).decode("utf-8")
+                    review = content[:500]
 
-                for_hash = author + title + message_id
+                    for_hash = author + title + message_id
 
-                news_item = NewsItemData(
-                    uuid.uuid4(),
-                    hashlib.sha256(for_hash.encode()).hexdigest(),
-                    title,
-                    review,
-                    url,
-                    link,
-                    published,
-                    author,
-                    datetime.datetime.now(),
-                    content,
-                    source.id,
-                    attributes,
-                )
+                    news_item = NewsItemData(
+                        uuid.uuid4(),
+                        hashlib.sha256(for_hash.encode()).hexdigest(),
+                        title,
+                        review,
+                        address,
+                        link,
+                        published,
+                        author,
+                        datetime.datetime.now(),
+                        content,
+                        source.id,
+                        attributes,
+                    )
+                    break
 
-                if part.get_content_maintype() == "multipart":
-                    pass
-                if part.get("Content-Disposition") is None:
-                    pass
+            if news_item:
+                for part in email_message.walk():
+                    file_name = part.get_filename()
+                    if file_name:
+                        binary_mime_type = part.get_content_type()
 
-                file_name = part.get_filename()
-
-                if file_name:
-
-                    binary_mime_type = part.get_content_type()
-                    binary_value = part.get_payload()
-
-                    news_attribute = NewsItemAttribute(uuid.uuid4(), key, value, binary_mime_type, binary_value)
-                    news_item.attributes.append(news_attribute)
+                        if binary_mime_type == "message/rfc822":
+                            logger.debug(f"{self.collector_source} Found an attached email")
+                            attached = part.get_payload()
+                            if isinstance(attached, list):
+                                attached_email = attached[0]
+                            else:
+                                attached_email = attached
+                            # Process .eml file as an email
+                            process_email(attached_email)
+                        else:
+                            # Handle other binary attachments
+                            logger.debug(f"{self.collector_source} Found an attachment '{file_name}' with MIME type '{binary_mime_type}'")
+                            binary_value = part.get_payload()
+                            if binary_value:
+                                news_attribute = NewsItemAttribute(key, "", binary_value, binary_mime_type)
+                                news_item.attributes.append(news_attribute)
+                            else:
+                                logger.error(f"{self.collector_source} Attachment is empty or could not be decoded: {file_name}")
 
                 news_items.append(news_item)
 
         if email_server_type.lower() == "imap":
+            logger.debug(f"{self.collector_source} Fetching emails using IMAP")
             try:
                 if proxy_server:
                     proxy_tunnel()
@@ -151,13 +162,15 @@ class EmailCollector(BaseCollector):
                     raw_email_string = raw_email.decode("utf-8")
                     email_message = email.message_from_string(raw_email_string, policy=policy.default)
 
-                    get_data()
+                    process_email(email_message)
 
                 connection.close()
                 connection.logout()
             except Exception as error:
                 logger.exception(f"{self.collector_source} Failed to fetch emails using IMAP: {error}")
+
         elif email_server_type.lower() == "pop3":
+            logger.debug(f"{self.collector_source} Fetching emails using POP3")
             try:
                 if proxy_server:
                     proxy_tunnel()
@@ -174,7 +187,7 @@ class EmailCollector(BaseCollector):
                     raw_email = b"\n".join(connection.retr(i + 1)[1])
                     email_message = email.message_from_bytes(raw_email)
 
-                    get_data()
+                    process_email(email_message)
 
                 connection.quit()
             except Exception as error:
