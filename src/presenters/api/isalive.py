8542c1132733d15dc849414e5202adb80803f844
multiflexi <multi.flexi@seznam.cz>
2025-09-08 11:06:00 +0200

    minor fixes

--- src/presenters/api/isalive.py
+++ src/presenters/api/isalive.py
@@ -1,11 +1,16 @@
-from flask_restful import Resource
+"""API endpoint to check if the service is alive."""
+
+from flask_restful import Api, Resource
 
 
 class IsAlive(Resource):
+    """API endpoint to check if the service is alive."""
 
-    def get(self):
-        return {'isalive': True}
+    def get(self) -> dict:
+        """Check if the service is alive."""
+        return {"isalive": True}
 
 
-def initialize(api):
+def initialize(api: Api) -> None:
+    """Initialize the API endpoint."""
     api.add_resource(IsAlive, "/api/v1/isalive")
--- src/presenters/managers/api_manager.py
+++ src/presenters/managers/api_manager.py
@@ -1,9 +1,18 @@
+"""API manager to initialize all API endpoints."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from api import isalive, presenters
 from flask_restful import Api
 
-from api import *
+if TYPE_CHECKING:
+    import Flask
 
 
-def initialize(app):
+def initialize(app: Flask) -> None:
+    """Initialize all API endpoints."""
     api = Api(app)
 
     isalive.initialize(api)
