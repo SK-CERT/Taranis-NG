SK-CERT/Taranis-NG

commit 46460d2dfefa71bfdcf553b2787e68fe410631f3

Asses UI improvements (#986)

src/shared/shared/schema/news_item.py
@@ -161,6 +161,8 @@ class NewsItemDataPresentationSchema(NewsItemDataBaseSchema):
     """Schema for presenting news item data."""
 
     remote_source = fields.Str()
+    osint_source_name = fields.Str()
+    osint_source_type = fields.Str()
     content = fields.Str()
     attributes = fields.Nested(NewsItemAttributeBaseSchema, many=True)
 
@@ -201,14 +203,14 @@ class NewsItemTagSchema(Schema):
     n_i_d = fields.Nested(NewsItemDataSchema, many=True)
 
 
-class NewsItemPresentationSchema(NewsItemBaseSchema, ACLEntryStatusSchema):
-    """Schema for presenting a news item with ACL entry status."""
-
-
 class NewsItemSchema(NewsItemBaseSchema):
     """Schema for a news item."""
 
     news_item_data = fields.Nested(NewsItemDataPresentationSchema)
+
+
+class NewsItemPresentationSchema(NewsItemSchema, ACLEntryStatusSchema):
+    """Schema for presenting a news item with ACL entry status."""
 
 
 class NewsItemAggregateSchema(Schema):

src/core/model/news_item.py
@@ -164,6 +164,16 @@ class NewsItemData(db.Model):
         """Return plain text version of content."""
         return strip_html(self.content) if self.content else ""
 
+    @property
+    def osint_source_name(self) -> str:
+        """Return OSINT source name."""
+        return self.osint_source.name if self.osint_source else ""
+
+    @property
+    def osint_source_type(self) -> str:
+        """Return OSINT source collector type."""
+        return self.osint_source.collector.name if self.osint_source and self.osint_source.collector else ""
+
     @classmethod
     def allowed_with_acl(cls, news_item_data_id: str, user: User, see: bool, access: bool, modify: bool) -> bool:
         """Check if user is allowed to access news item data.

src/gui/src/components/analyze/ToolbarFilterAnalyze.vue
@@ -160,12 +160,6 @@
                 }, 300);
             },
 
-            changeTheme() {
-                this.$vuetify.theme.themes.light.primary = "#f0f";
-                this.$vuetify.theme.themes.light.secondary = '#f00';
-                this.$vuetify.theme.themes.light.bg = '#0f0';
-                this.$vuetify.theme.themes.light.base = '#00f';
-            },
             remove(item) {
                 this.chips.splice(this.chips.indexOf(item), 1);
                 this.chips = [...this.chips]

src/gui/src/components/assess/CardAssessItem.vue
@@ -6,21 +6,21 @@
                     <v-checkbox v-if="!analyze_selector" v-model="selected" @change="selectionChanged"></v-checkbox>
                 </v-row>
             </v-col>
-
             <v-col :class="UI.CLASS.card_offset">
-                <v-hover v-slot="{hover}">
-                    <v-card v-bind="UI.CARD.HOVER" :elevation="hover ? 12 : 2"
-                            @click.stop="cardItemToolbar"
-                            @mouseenter.native="toolbar=true"
-                            @mouseleave.native="toolbar=cardFocus"
+                <v-hover v-slot="{ hover }">
+                    <v-card v-bind="UI.CARD.HOVER" :elevation="hover ? 12 : 2" @click.stop="cardItemToolbar"
+                            @mouseenter.native="toolbar = true" @mouseleave.native="toolbar = cardFocus"
                             :color="selectedColor">
                         <!--CONTENT-->
-                        <v-layout v-bind="UI.CARD.LAYOUT" :class="'status ' + cardStatus">
+                        <v-layout v-bind="UI.CARD.LAYOUT" :class="['status', cardStatus, { 'read': isRead }]">
                             <v-row v-bind="UI.CARD.ROW.CONTENT">
-                                <!--COLLECTED, PUBLISHED, SOURCE-->
                                 <v-col v-bind="UI.CARD.COL.INFO">
                                     <div>
-                                        {{ $t('card_item.collected') }}: <strong>{{ news_item.news_item_data.collected }}</strong>
+                                        {{ $t('card_item.source') }}:
+                                        <strong>
+                                            {{ news_item.news_item_data.osint_source_name || news_item.news_item_data.source }}
+                                            ({{ news_item.news_item_data.osint_source_type.split(' ')[0] }})
+                                        </strong>
                                     </div>
                                 </v-col>
                                 <v-col v-bind="UI.CARD.COL.INFO">
@@ -31,74 +31,85 @@
                                 </v-col>
                                 <v-col v-bind="UI.CARD.COL.INFO">
                                     <div align="right">
-                                        {{ $t('card_item.source') }}:
-                                        <strong>{{ news_item.news_item_data.source }}</strong>
+                                        {{ $t('card_item.collected') }}:
+                                        <strong>{{ news_item.news_item_data.collected }}</strong>
                                     </div>
                                 </v-col>
-
-                                <!--TITLE-->
                                 <v-col v-bind="UI.CARD.COL.TITLE">
-                                    <div v-if="word_list_regex" v-html="wordCheck(news_item.news_item_data.title)"></div>
+                                    <div v-if="word_list_regex" v-html="wordCheck(news_item.news_item_data.title)">
+                                    </div>
                                     <div v-else>{{ news_item.news_item_data.title }}</div>
                                 </v-col>
-
-                                <!--REVIEW-->
                                 <v-col v-bind="UI.CARD.COL.REVIEW" class="review-content">
                                     <div v-if="!compact_mode">
-                                        <div v-if="word_list_regex" v-html="wordCheck(news_item.news_item_data.review)"></div>
+                                        <div v-if="word_list_regex" v-html="wordCheck(news_item.news_item_data.review)">
+                                        </div>
                                         <div v-else>{{ news_item.news_item_data.review }}</div>
                                     </div>
                                 </v-col>
-
-                                <!--FOOTER-->
                                 <v-row v-bind="UI.CARD.FOOTER">
-                                    <v-col cols="11">
-                                        <span v-if="canAccess" class="caption font-weight-bold px-0 mt-1 pb-0 pt-0 info--text source-link">
+                                    <v-col cols="11" class="footer-content">
+                                        <span class="caption font-weight-bold px-0 mt-1 pb-0 pt-0 info--text source-link">
                                             {{ news_item.news_item_data.link }}
-                                        </span>
-
-                                        <span class="caption font-weight-bold grey--text pl-2 pr-1">
-                                            <v-icon color="grey" size="12">mdi-thumb-up</v-icon> {{ news_item.likes }}
-                                        </span>
-
-                                        <span class="caption font-weight-bold grey--text pl-1 pr-2">
-                                            <v-icon color="grey" size="12">mdi-thumb-down</v-icon> {{ news_item.dislikes }}
                                         </span>
                                     </v-col>
 
-                                    <!--HOVER TOOLBAR-->
+                                    <!--TOOLBAR-->
                                     <v-col cols="1">
-                                        <v-row v-if="!multiSelectActive && !analyze_selector && hover"
-                                               :style="UI.STYLE.card_toolbar">
-                                            <v-col v-bind="UI.CARD.COL.TOOLS" :style="UI.STYLE.card_toolbar_strip_bottom">
-                                                <v-btn v-if="canModify" icon @click.stop="cardItemToolbar('ungroup')" data-btn="ungroup" :title="$t('assess.tooltip.ungroup_item')">
+                                        <v-row v-if="!multiSelectActive && !analyze_selector"
+                                                v-bind="UI.CARD.TOOLBAR.COMPACT"
+                                                :style="UI.STYLE.card_toolbar_strip_bottom">
+                                            <v-col v-bind="UI.CARD.COL.TOOLS">
+                                                <span class="ml-5 grey--text">
+                                                    <v-btn v-if="canModify" icon @click.stop="cardItemToolbar('like')"
+                                                           data-btn="like" :title="$t('assess.tooltip.like_item')">
+                                                        <v-icon :color="buttonStatus(news_item.me_like)">
+                                                            {{ news_item.likes ? 'mdi-thumb-up' : 'mdi-thumb-up-outline' }}
+                                                        </v-icon>
+                                                    </v-btn>
+                                                    {{ news_item.likes }}
+                                                </span>
+
+                                                <span class="mr-5 grey--text">
+                                                    <v-btn v-if="canModify" icon @click.stop="cardItemToolbar('unlike')"
+                                                           data-btn="unlike" :title="$t('assess.tooltip.dislike_item')">
+                                                        <v-icon :color="buttonStatus(news_item.me_dislike)">
+                                                            {{ news_item.dislikes ? 'mdi-thumb-down' : 'mdi-thumb-down-outline' }}
+                                                        </v-icon>
+                                                    </v-btn>
+                                                    {{ news_item.dislikes }}
+                                                </span>
+
+                                                <v-btn v-if="canModify" icon @click.stop="cardItemToolbar('ungroup')"
+                                                       data-btn="ungroup" :title="$t('assess.tooltip.ungroup_item')">
                                                     <v-icon color="primary">mdi-ungroup</v-icon>
                                                 </v-btn>
 
-                                                <v-btn v-if="canAccess" icon @click.stop="cardItemToolbar('link')" data-btn="link" :title="$t('assess.tooltip.open_source')">
-                                                    <a class="alink" :href="news_item.news_item_data.link" target="_blank" rel="noreferer">
+                                                <v-btn icon @click.stop="cardItemToolbar('link')"
+                                                       data-btn="link" :title="$t('assess.tooltip.open_source')">
+                                                    <a class="alink" :href="news_item.news_item_data.link"
+                                                       target="_blank" rel="noreferer">
                                                         <v-icon color="primary">mdi-open-in-app</v-icon>
                                                     </a>
                                                 </v-btn>
 
-                                                <v-btn v-if="canModify" icon @click.stop="cardItemToolbar('read')" data-btn="read" :title="$t('assess.tooltip.read_item')">
-                                                    <v-icon :color="buttonStatus(news_item.read)">mdi-eye</v-icon>
+                                                <v-btn v-if="canModify" icon @click.stop="cardItemToolbar('read')"
+                                                       data-btn="read" :title="$t('assess.tooltip.read_item')">
+                                                    <v-icon :color="buttonStatus(news_item.read)">
+                                                        {{ news_item.read ? 'mdi-eye' : 'mdi-eye-outline' }}
+                                                    </v-icon>
                                                 </v-btn>
 
-                                                <v-btn v-if="canModify" icon @click.stop="cardItemToolbar('important')" data-btn="important" :title="$t('assess.tooltip.important_item')">
-                                                    <v-icon :color="buttonStatus(news_item.important)">mdi-star</v-icon>
+                                                <v-btn v-if="canModify" icon @click.stop="cardItemToolbar('important')"
+                                                       data-btn="important" :title="$t('assess.tooltip.important_item')">
+                                                    <v-icon :color="buttonStatus(news_item.important)">
+                                                        {{ news_item.important ? 'mdi-star' : 'mdi-star-outline' }}
+                                                    </v-icon>
                                                 </v-btn>
 
-                                                <v-btn v-if="canModify" icon @click.stop="cardItemToolbar('like')" data-btn="like" :title="$t('assess.tooltip.like_item')">
-                                                    <v-icon :color="buttonStatus(news_item.me_like)">mdi-thumb-up</v-icon>
-                                                </v-btn>
-
-                                                <v-btn v-if="canModify" icon @click.stop="cardItemToolbar('unlike')" data-btn="unlike" :title="$t('assess.tooltip.dislike_item')">
-                                                    <v-icon :color="buttonStatus(news_item.me_dislike)">mdi-thumb-down</v-icon>
-                                                </v-btn>
-
-                                                <v-btn v-if="canDelete" icon @click.stop="showMsgBox" data-btn="delete" :title="$t('assess.tooltip.delete_item')">
-                                                    <v-icon color="primary">mdi-delete</v-icon>
+                                                <v-btn v-if="canDelete" icon @click.stop="showMsgBox" data-btn="delete"
+                                                       :title="$t('assess.tooltip.delete_item')">
+                                                    <v-icon color="error">mdi-delete-outline</v-icon>
                                                 </v-btn>
 
                                             </v-col>
@@ -112,12 +123,8 @@
             </v-col>
         </v-row>
         <v-row>
-            <MessageBox v-model="msgbox_visible"
-                        @yes="handleMsgBox"
-                        @cancel="msgbox_visible = false"
-                        :title="$t('common.messagebox.delete')"
-                        :message="news_item.news_item_data.title"
-                        :alert=true>
+            <MessageBox v-model="msgbox_visible" @yes="handleMsgBox" @cancel="msgbox_visible = false"
+                        :title="$t('common.messagebox.delete')" :message="news_item.news_item_data.title" :alert=true>
             </MessageBox>
         </v-row>
     </v-container>
@@ -131,6 +138,7 @@
     import AuthMixin from "@/services/auth/auth_mixin";
     import Permissions from "@/services/auth/permissions";
     import MessageBox from "@/components/common/MessageBox.vue";
+    import CardMixin from "@/components/assess/card_mixin";
 
     export default {
         name: "CardAssessItem",
@@ -141,54 +149,15 @@
             compact_mode: Boolean,
             word_list_regex: String
         },
-        mixins: [AuthMixin],
-        data: () => ({
-            toolbar: false,
-            selected: false,
-            msgbox_visible: false,
-        }),
+        mixins: [AuthMixin, CardMixin],
+        data: () => ({}),
         computed: {
-            canAccess() {
-                return this.checkPermission(Permissions.ASSESS_ACCESS) && this.news_item.access === true
-            },
-
             canModify() {
                 return this.checkPermission(Permissions.ASSESS_UPDATE) && this.news_item.modify === true
             },
 
             canDelete() {
                 return this.checkPermission(Permissions.ASSESS_DELETE) && this.news_item.modify === true
-            },
-
-            canCreateReport() {
-                return this.checkPermission(Permissions.ANALYZE_CREATE)
-            },
-
-            multiSelectActive() {
-                return this.$store.getters.getMultiSelect
-            },
-            selectedColor() {
-                if (this.selected === true) {
-                    return this.$vuetify.theme.dark ? "blue-grey darken-3" : "orange lighten-4"
-                } else {
-                    return ""
-                }
-            },
-            cardFocus() {
-                if (this.$el.querySelector(".card .layout").classList.contains('focus')) {
-                    return true;
-                } else {
-                    return false;
-                }
-            },
-            cardStatus() {
-                if (this.news_item.important) {
-                    return "important"
-                } else if (this.news_item.read) {
-                    return "read"
-                } else {
-                    return "new"
-                }
             },
         },
         methods: {
@@ -198,6 +167,7 @@
                     this.stateChange();
                 }
             },
+
             selectionChanged() {
                 if (this.selected === true) {
                     this.$store.dispatch("select", { 'type': 'ITEM', 'id': this.news_item.id, 'item': this.news_item })
@@ -205,20 +175,7 @@
                     this.$store.dispatch("deselect", { 'type': 'ITEM', 'id': this.news_item.id, 'item': this.news_item })
                 }
             },
-            stateChange() {
-                this.$root.$emit('change-state', 'SHOW_ITEM');
-                this.$root.$emit('check-focus', this.$el.dataset.id);
-                this.$root.$emit('update-pos', parseInt(this.$el.dataset.id));
-            },
-            getGroupId() {
-                if (window.location.pathname.includes("/group/")) {
-                    let i = window.location.pathname.indexOf("/group/");
-                    let len = window.location.pathname.length;
-                    return window.location.pathname.substring(i + 7, len);
-                } else {
-                    return null;
-                }
-            },
+
             cardItemToolbar(action) {
                 switch (action) {
                     case "like":
@@ -257,6 +214,8 @@
                         break;
 
                     case "ungroup":
+                        // Emit event to parent to close the expanded aggregate view
+                        this.$parent.opened = false;
                         groupAction({
                             'group': this.getGroupId(),
                             'action': 'UNGROUP',
@@ -278,72 +237,30 @@
                         break;
                 }
             },
-
-            buttonStatus: function (active) {
-                if (active) {
-                    return "amber darken-2"
-                } else {
-                    return "primary"
-                }
-            },
-            wordCheck(target) {
-                let parse = new Array();
-                let message = this.escapeHtml(target).split(' ');
-                let word_list = new RegExp(this.word_list_regex, "gi");
-
-                for (let i = 0; i < message.length; i++) {
-                    let res = message[i].replace(word_list, function (x) {
-                        return "<span class='wordlist'>" + x + "</span>";
-                    });
-
-                    parse.push(res + " ");
-                }
-
-                return parse.join('');
-            },
-            escapeHtml(text) {
-                let map = {
-                    '&': '&amp;',
-                    '<': '&lt;',
-                    '>': '&gt;',
-                    '"': '&quot;',
-                    "'": '&#039;'
-                };
-
-                return text.replace(/[&<>"']/g, function (m) { return map[m]; });
-            },
-            multiSelectOff() {
-                this.selected = false
-            },
-            setFocus(id) {
-                if (this.$el.dataset.id == id) {
-                    this.toolbar = true;
-                    this.$el.querySelector(".card .layout").classList.add('focus');
-                } else {
-                    this.toolbar = false;
-                    this.$el.querySelector(".card .layout").classList.remove('focus');
-                }
-            },
-            showMsgBox() {
-                this.msgbox_visible = true;
-            },
-            handleMsgBox() {
-                this.msgbox_visible = false;
-                this.cardItemToolbar('delete')
-            }
-        },
-        mounted() {
-            this.$root.$on('multi-select-off', this.multiSelectOff);
-
-            this.$root.$on('check-focus', (id) => {
-                this.setFocus(id);
-            });
-        },
-        beforeDestroy() {
-            this.$root.$off('multi-select-off', this.multiSelectOff);
         }
     }
 </script>
+<style scoped>
+    .footer-content {
+        min-height: 48px;
+    }
+
+    .status.read {
+        opacity: 0.5;
+    }
+
+    .theme--light .status.read {
+        background-color: #f5f5f5;
+    }
+
+    .theme--dark .status.read {
+        background-color: #2c2c2c;
+    }
+
+    .status.read .v-card {
+        filter: grayscale(30%);
+    }
+</style>
 <style>
     .v-dialog--fullscreen {
         position: fixed !important;

src/gui/src/components/assess/ContentDataAssess.vue
@@ -1,8 +1,8 @@
 <template>
 
     <v-container class="selector_assess" :id="selfID">
-        <component v-bind:is="cardLayout()" v-for="(news_item,i) in news_items_data" :card="news_item"
-                   :key="i" :analyze_selector="analyze_selector"
+        <component v-bind:is="cardLayout()" v-for="news_item in news_items_data" :card="news_item"
+                   :key="news_item.id" :analyze_selector="analyze_selector"
                    :preselected="preselected(news_item.id)"
                    :word_list_regex="regexWordList" :data_set="data_set"
                    @show-single-aggregate-detail="showSingleAggregateDetail(news_item)"
@@ -48,7 +48,7 @@
             filter: {
                 search: "",
                 range: "ALL",
-                read: false,
+                read: true,
                 important: false,
                 relevant: false,
                 sort: "DATE_DESC"
@@ -134,7 +134,7 @@
                     if ((append === false) || reload_all) {
                         this.news_items_data = []
                     }
-                    // console.log('#', "Add:", append, "Reload:", reload_all, "O:", offset, "L:", limit, "->", this.$store.getters.getNewsItems.items.length);
+                    // console.log('#', "Add:", append, "Reload:", reload_all, "Opened:", this.aggregate_open.length, "O:", offset, "L:", limit, "->", this.$store.getters.getNewsItems.items.length);
                     if (append) {
                         this.news_items_data = this.news_items_data.concat(this.$store.getters.getNewsItems.items);
                     } else {
@@ -153,11 +153,11 @@
 
             updateFilter(filter) {
                 this.filter = filter;
-                this.updateData(false, false);
+                this.updateData(false, true);
             },
 
             setAggregateOpen(folder) {
-
+                // TODO (JP): looks like aggregate_open[] is not used, check it deeper, it looks .oppened is handled inside Cards
                 if (!this.aggregate_open.length) {
                     this.aggregate_open.push(folder.id);
                 } else if (folder.opened === false) {
@@ -180,7 +180,7 @@
             },
 
             aggregateOpen(folder) {
-
+                // TODO (JP): looks like aggregate_open[] is not used, check it deeper
                 for (let i = 0; i < this.aggregate_open.length; i++) {
                     if (this.aggregate_open[i] == folder.id && folder.news_items.length !== 1) {
                         return true;

commit 139cee7080092f74bbf98d91efd8e534d921c185

fixed: remote_source can't be null in Presenter (caused by #986 Asses UI improvements)

src/shared/shared/schema/news_item.py
@@ -160,7 +160,7 @@ class NewsItemRemoteSchema(Schema):
 class NewsItemDataPresentationSchema(NewsItemDataBaseSchema):
     """Schema for presenting news item data."""
 
-    remote_source = fields.Str()
+    remote_source = fields.Str(allow_none=True)
     osint_source_name = fields.Str()
     osint_source_type = fields.Str()
     content = fields.Str()