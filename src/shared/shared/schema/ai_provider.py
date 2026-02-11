03ef457ebd5d2e9472641ea61f99a8c532f473f2
Progress <Progress1@users.noreply.github.com>
2026-02-11 20:41:33 +0100

    Fixed error in github actions (#1057)
    - some change just to check if it works
    - pre-commit hook incerase ruff to v0.15.0

--- src/shared/shared/schema/ai_provider.py
+++ src/shared/shared/schema/ai_provider.py
@@ -1,6 +1,37 @@
-"""This module defines the schema and data model for local AI models using Marshmallow for serialization and deserialization."""
+"""This module defines the schema and data model for AI model."""
 
 from marshmallow import EXCLUDE, Schema, fields, post_load
+
+
+class AiProvider:
+    """Data model for AI model."""
+
+    def __init__(
+        self,
+        id: int,  # noqa: A002
+        name: str,
+        api_type: str,
+        api_url: str,
+        api_key: str,
+        model: str,
+    ) -> None:
+        """Initialize an AiProvider instance.
+
+        Args:
+            id (int): Unique identifier for the AI model.
+            name (str): Name of the AI model.
+            api_type (str): API type ("openai").
+            api_url (str): The endpoint URL.
+            api_key (str): API key for authentication.
+            model (str): Model name or identifier.
+        """
+        self.id = id
+        self.name = name
+        self.api_type = api_type
+        self.api_url = api_url
+        self.api_key = api_key
+        self.model = model
+        # updated_by and updated_at are set on the server
 
 
 class AiProviderSchema(Schema):
@@ -21,37 +52,14 @@ class AiProviderSchema(Schema):
     updated_at = fields.Str(dump_only=True)
 
     @post_load
-    def make(self, data, **kwargs):
+    def make(self, data: dict, **kwargs) -> AiProvider:  # noqa: ANN003, ARG002
         """Create a AiProvider instance from the deserialized data.
 
         Args:
-            data (dict): The deserialized data containing local AI modelr attributes.
+            data (dict): The deserialized data containing AI model attributes.
             **kwargs: Additional keyword arguments.
 
         Returns:
             AiProvider: An instance of AiProvider initialized with the provided data.
         """
         return AiProvider(**data)
-
-
-class AiProvider:
-    """Data model for Local AI models."""
-
-    def __init__(self, id, name, api_type, api_url, api_key, model):
-        """Initialize an AiProvider instance.
-
-        Args:
-            id (int): Unique identifier for the local AI model.
-            name (str): Name of the local AI model.
-            api_type (str): API type ("openai").
-            api_url (str): The endpoint URL.
-            api_key (str): API key for authentication.
-            model (str): Model name or identifier.
-        """
-        self.id = id
-        self.name = name
-        self.api_type = api_type
-        self.api_url = api_url
-        self.api_key = api_key
-        self.model = model
-        # updated_by and updated_at are set on the server
