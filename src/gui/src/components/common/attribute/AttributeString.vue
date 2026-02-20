f36686d0aa974112b65f1e4cf207b9fcd608e3c3
Jaroslav Svoboda <multiflexi@users.noreply.github.com>
2026-02-20 10:35:37 +0100

    Add numbering to string attributes (#1074)
    * add numbering to string attributes
    - centered horizontal (number was below text)
    - show only if > 1
    - little bit lighter color to be more low-priority on the screen
    - color is now dark/light theme independent

--- src/gui/src/components/common/attribute/AttributeString.vue
+++ src/gui/src/components/common/attribute/AttributeString.vue
@@ -1,59 +1,31 @@
 <template>
-
-    <!--<div>
-        <v-row v-for="(value, index) in values" :key="value.index"
-               class="valueHolder"
-               @mouseenter="delButton=true"
-               @mouseleave="delButton=false"
-        >
-
-            <v-col cols="11">
-                <span v-if="read_only || values[index].remote">{{values[index].value}}</span>
-                <v-text-field v-if="!read_only && !values[index].remote" v-model="values[index].value" :label="$t('attribute.value')"
-                              @focus="onFocus(index)" @blur="onBlur(index)" @keyup="onKeyUp(index)"
-                              :class="getLockedStyle(index)"
-                              :disabled="values[index].locked || !canModify"></v-text-field>
-            </v-col>
-
-            <v-col v-if="!read_only && canModify && !values[index].remote" class="mt-6" cols="1">
-                <v-btn v-if="delButton" text small @click="del(index)">
-                    <v-icon>mdi-close-circle</v-icon>
-                </v-btn>
-            </v-col>
-
-        </v-row>
-
-        <v-btn v-if="values.length < attribute_group.max_occurrence && !read_only && canModify" depressed small @click="add">
-            <v-icon>mdi-plus</v-icon>
-        </v-btn>
-
-    </div>-->
-    <AttributeItemLayout
-            :add_button="addButtonVisible"
-            @add-value="add()"
-            :values="values"
-    >
+    <AttributeItemLayout :add_button="addButtonVisible"
+                         @add-value="add()"
+                         :values="values">
         <template v-slot:content>
             <v-row v-for="(value, index) in values" :key="value.index"
-                   class="valueHolder"
-            >
-                <span v-if="read_only || values[index].remote">{{values[index].value}}</span>
-                <AttributeValueLayout
-                        v-if="!read_only && canModify && !values[index].remote"
-                        :del_button="delButtonVisible"
-                        @del-value="del(index)"
-                        :occurrence="attribute_group.min_occurrence"
-                        :values="values"
-                        :val_index="index"
-                >
+                   class="valueHolder">
+                <span v-if="read_only || values[index].remote" class="numbered-string-value">
+                    <span v-if="values.length > 1" class="string-number text--disabled">{{ index + 1 }}.</span>
+                    <span class="string-content">{{values[index].value}}</span>
+                </span>
+                <AttributeValueLayout v-if="!read_only && canModify && !values[index].remote"
+                                      :del_button="delButtonVisible"
+                                      @del-value="del(index)"
+                                      :occurrence="attribute_group.min_occurrence"
+                                      :values="values"
+                                      :val_index="index">
+                    <template v-slot:col_left>
+                        <span v-if="values.length > 1" class="string-number text--disabled">{{ index + 1 }}.</span>
+                    </template>
                     <template v-slot:col_middle>
                         <v-text-field v-if="!read_only && !values[index].remote"
                                       v-model="values[index].value" dense
                                       :label="$t('attribute.value')"
                                       @focus="onFocus(index)" @blur="onBlur(index)" @keyup="onKeyUp(index)"
                                       :class="getLockedStyle(index)"
-                                      :disabled="values[index].locked || !canModify"
-                        ></v-text-field>
+                                      :disabled="values[index].locked || !canModify">
+                        </v-text-field>
                     </template>
                 </AttributeValueLayout>
             </v-row>
@@ -79,3 +51,23 @@
         mixins: [AttributesMixin]
     }
 </script>
+
+<style scoped>
+    .string-number {
+        margin-right: 8px;
+        user-select: none;
+        min-width: 24px;
+        display: inline-block;
+    }
+
+    .numbered-string-value {
+        display: flex;
+        align-items: center;
+        width: 100%;
+    }
+
+    .string-content {
+        flex: 1;
+        word-break: break-word;
+    }
+</style>
