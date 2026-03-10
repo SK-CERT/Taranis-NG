398855a38cfef1a3a19288c349aa20368648ded5
Jan Polonsky <jan.polonsky@nbu.gov.sk>
2026-03-10 14:31:05 +0100

    -hide JWT token in download action for news item attribute (no jwt token in Traefik, Core logs) - it's part of the changes for CESNET pentest: Access token in url (is saved to logs) CESNET pentest: Access token in url (is saved to logs) #726 (other changes solving this problem will come...) - fixed bug in news items: Type check failed for prop "href". Expected String, Object, got Function - fixed NewsItemData.allowed_with_acl() got an unexpected keyword argument 'delete'

--- src/core/api/assess.py
+++ src/core/api/assess.py
@@ -5,11 +5,12 @@ from http import HTTPStatus
 
 from flask import request, send_file
 from flask_restful import Resource
-from managers import auth_manager, log_manager
+from managers import auth_manager
 from managers.auth_manager import ACLCheck, auth_required
 from managers.log_manager import logger
 from managers.sse_manager import sse_manager
-from model import news_item, osint_source
+from model.news_item import NewsItem, NewsItemAggregate, NewsItemAttribute
+from model.osint_source import OSINTSource, OSINTSourceGroup
 from model.permission import Permission
 
 
@@ -19,7 +20,7 @@ class OSINTSourceGroupsAssess(Resource):
     @auth_required("ASSESS_ACCESS")
     def get(self) -> dict:
         """Get all OSINT source groups for Assess."""
-        return osint_source.OSINTSourceGroup.get_all_json(None, auth_manager.get_user_from_jwt(), acl_check=True)
+        return OSINTSourceGroup.get_all_json(None, auth_manager.get_user_from_jwt(), acl_check=True)
 
 
 class ManualOSINTSources(Resource):
@@ -28,7 +29,7 @@ class ManualOSINTSources(Resource):
     @auth_required(["ASSESS_ACCESS"])
     def get(self) -> dict:
         """Get all manual OSINT sources for Assess."""
-        return osint_source.OSINTSource.get_all_manual_json(auth_manager.get_user_from_jwt())
+        return OSINTSource.get_all_manual_json(auth_manager.get_user_from_jwt())
 
 
 class AddNewsItem(Resource):
@@ -37,7 +38,7 @@ class AddNewsItem(Resource):
     @auth_required("ASSESS_CREATE")
     def post(self) -> None:
         """Add a news item."""
-        osint_source_ids = news_item.NewsItemAggregate.add_news_item(request.json)
+        osint_source_ids = NewsItemAggregate.add_news_item(request.json)
         sse_manager.news_items_updated()
         sse_manager.remote_access_news_items_updated(osint_source_ids)
 
@@ -84,10 +85,10 @@ class NewsItemsByGroup(Resource):
             logger.exception(f"{msg}: {ex}")
             return {"error": msg}, HTTPStatus.INTERNAL_SERVER_ERROR
 
-        return news_item.NewsItemAggregate.get_by_group_json(group_id, filters, offset, limit, user)
+        return NewsItemAggregate.get_by_group_json(group_id, filters, offset, limit, user)
 
 
-class NewsItem(Resource):
+class NewsItemResource(Resource):
     """News item API endpoint."""
 
     @auth_required("ASSESS_ACCESS", ACLCheck.NEWS_ITEM_ACCESS)
@@ -99,7 +100,7 @@ class NewsItem(Resource):
         Returns:
             (dict): The news item
         """
-        return news_item.NewsItem.get_detail_json(item_id)
+        return NewsItem.get_detail_json(item_id)
 
     @auth_required("ASSESS_UPDATE", ACLCheck.NEWS_ITEM_MODIFY)
     def put(self, item_id: str) -> tuple[dict, int]:
@@ -112,7 +113,7 @@ class NewsItem(Resource):
             (int): The response code
         """
         user = auth_manager.get_user_from_jwt()
-        response, osint_source_ids, code = news_item.NewsItem.update(item_id, request.json, user.id)
+        response, osint_source_ids, code = NewsItem.update(item_id, request.json, user.id)
         sse_manager.news_items_updated()
         if osint_source_ids:
             sse_manager.remote_access_news_items_updated(osint_source_ids)
@@ -128,12 +129,12 @@ class NewsItem(Resource):
             (dict): The response
             (int): The response code
         """
-        response, code = news_item.NewsItem.delete(item_id)
+        response, code = NewsItem.delete(item_id)
         sse_manager.news_items_updated()
         return response, code
 
 
-class NewsItemAggregate(Resource):
+class NewsItemAggregateResource(Resource):
     """News item aggregate API endpoint."""
 
     @auth_required("ASSESS_UPDATE")
@@ -147,7 +148,7 @@ class NewsItemAggregate(Resource):
             (int): The response code
         """
         user = auth_manager.get_user_from_jwt()
-        response, osint_source_ids, code = news_item.NewsItemAggregate.update(aggregate_id, request.json, user)
+        response, osint_source_ids, code = NewsItemAggregate.update(aggregate_id, request.json, user)
         sse_manager.news_items_updated()
         if osint_source_ids:
             sse_manager.remote_access_news_items_updated(osint_source_ids)
@@ -164,7 +165,7 @@ class NewsItemAggregate(Resource):
             (int): The response code
         """
         user = auth_manager.get_user_from_jwt()
-        response, code = news_item.NewsItemAggregate.delete(aggregate_id, user)
+        response, code = NewsItemAggregate.delete(aggregate_id, user)
         sse_manager.news_items_updated()
         return response, code
 
@@ -185,9 +186,9 @@ class GroupAction(Resource):
         osint_source_ids = set()
 
         if action == "DELETE":
-            response, code = news_item.NewsItemAggregate.group_action_delete(request.json, user)
+            response, code = NewsItemAggregate.group_action_delete(request.json, user)
         else:
-            response, osint_source_ids, code = news_item.NewsItemAggregate.group_action(request.json, user)
+            response, osint_source_ids, code = NewsItemAggregate.group_action(request.json, user)
 
         sse_manager.news_items_updated()
         if osint_source_ids:
@@ -198,7 +199,12 @@ class GroupAction(Resource):
 class DownloadAttachment(Resource):
     """Download attachment API endpoint."""
 
-    def get(self, item_data_id: str, attribute_id: str) -> None:
+    @auth_required("ASSESS_ACCESS")
+    def post(
+        self,
+        item_data_id: str,  # noqa: ARG002, auth_required ACLCheck is build for news_item (item_id, int), this is news_item_data (item_data_id, str)
+        attribute_id: str,
+    ) -> None:
         """Download attachment.
 
         Args:
@@ -208,42 +214,15 @@ class DownloadAttachment(Resource):
         Returns:
             (file): The file
         """
-        if "jwt" in request.args:
-            user = auth_manager.decode_user_from_jwt(request.args["jwt"])
-            if user is not None:
-                permissions = user.get_permissions()
-                if "ASSESS_ACCESS" in permissions:
-                    attribute_mapping = news_item.NewsItemDataNewsItemAttribute.find(attribute_id)
-                    need_check = False
-                    if attribute_mapping is not None:
-                        need_check = True
-                    attribute = news_item.NewsItemAttribute.find(attribute_id)
-                    if (
-                        need_check
-                        and item_data_id == attribute_mapping.news_item_data_id
-                        and news_item.NewsItemData.allowed_with_acl(
-                            attribute_mapping.news_item_data_id,
-                            user,
-                            see=False,
-                            modify=True,
-                            delete=False,
-                        )
-                    ):
-                        log_manager.store_user_activity(user, "ASSESS_ACCESS", str({"file": attribute.value}))
-                        return send_file(
-                            io.BytesIO(attribute.binary_data),
-                            download_name=attribute.value,
-                            mimetype=attribute.binary_mime_type,
-                            as_attachment=True,
-                        )
-                    log_manager.store_auth_error_activity("Unauthorized access attempt to News Item Data")
-                    return None
-                log_manager.store_auth_error_activity("Insufficient permissions")
-                return None
-            log_manager.store_auth_error_activity("Invalid JWT")
-            return None
-        log_manager.store_auth_error_activity("Missing JWT")
-        return None
+        attribute = NewsItemAttribute.find(attribute_id)
+        response = send_file(
+            io.BytesIO(attribute.binary_data),
+            download_name=attribute.value,
+            mimetype=attribute.binary_mime_type,
+            as_attachment=True,
+        )
+        response.headers["Access-Control-Expose-Headers"] = "Content-Disposition"
+        return response
 
 
 def initialize(api: object) -> None:
@@ -252,8 +231,8 @@ def initialize(api: object) -> None:
     api.add_resource(ManualOSINTSources, "/api/v1/assess/manual-osint-sources")
     api.add_resource(AddNewsItem, "/api/v1/assess/news-items")
     api.add_resource(NewsItemsByGroup, "/api/v1/assess/news-item-aggregates-by-group/<string:group_id>")
-    api.add_resource(NewsItem, "/api/v1/assess/news-items/<int:item_id>")
-    api.add_resource(NewsItemAggregate, "/api/v1/assess/news-item-aggregates/<int:aggregate_id>")
+    api.add_resource(NewsItemResource, "/api/v1/assess/news-items/<int:item_id>")
+    api.add_resource(NewsItemAggregateResource, "/api/v1/assess/news-item-aggregates/<int:aggregate_id>")
     api.add_resource(GroupAction, "/api/v1/assess/news-item-aggregates-group-action")
     api.add_resource(DownloadAttachment, "/api/v1/assess/news-item-data/<string:item_data_id>/attributes/<int:attribute_id>/file")
 
