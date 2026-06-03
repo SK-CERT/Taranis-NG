ad999c357249b5772687b981f22dec7b4201d322
multiflexi <multi.flexi@seznam.cz>
2026-06-03 08:59:43 +0200

    clean up login

--- src/gui-v3/src/env.d.ts
+++ src/gui-v3/src/env.d.ts
@@ -7,9 +7,6 @@ interface ImportMetaEnv {
     readonly VITE_APP_TARANIS_NG_LOGIN_URL?: string
     readonly VITE_APP_TARANIS_NG_LOGOUT_URL?: string
     readonly VITE_APP_TARANIS_NG_TESTING_TOKEN?: string
-    readonly VITE_APP_TARANIS_NG_LOGIN_METHOD?: string
-    readonly VITE_APP_TARANIS_NG_LOGOUT_METHOD?: string
-    readonly VITE_APP_TARANIS_NG_SSE_INIT_METHOD?: string
 }
 
 interface ImportMeta {
